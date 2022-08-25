07aa5774d6cada53260894f0228914daae90b85c
Gabriel Speckhahn <749488+gabspeck@users.noreply.github.com>
2022-08-25 23:29:12 +0100

    chore: format and ignore smui css

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,38 @@
-import {sveltekit} from '@sveltejs/kit/vite';
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import path from 'path';
-import inject from "@rollup/plugin-inject";
+import inject from '@rollup/plugin-inject';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()],
-    optimizeDeps: {
-        include: ['@solana/web3.js', 'buffer'],
-        esbuildOptions: {
-            target: 'esnext',
-            plugins: [NodeGlobalsPolyfillPlugin({buffer: true})],
-        },
-    },
-    resolve: {
-        alias: {
-            $utils: path.resolve('src/utils/'),
-            stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-        },
-    },
-    define: {
-        'process.env.BROWSER': true,
-        'process.env.NODE_DEBUG': JSON.stringify(''),
-    },
-    build: {
-        target: 'esnext',
-        commonjsOptions: {
-            transformMixedEsModules: true
-        },
-        rollupOptions: {
-            plugins: [inject({Buffer: ['buffer', 'Buffer']}), nodePolyfills({crypto: true})],
-        },
-    }
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['@solana/web3.js', 'buffer'],
+		esbuildOptions: {
+			target: 'esnext',
+			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
+		}
+	},
+	resolve: {
+		alias: {
+			$utils: path.resolve('src/utils/'),
+			stream: 'rollup-plugin-node-polyfills/polyfills/stream'
+		}
+	},
+	define: {
+		'process.env.BROWSER': true,
+		'process.env.NODE_DEBUG': JSON.stringify('')
+	},
+	build: {
+		target: 'esnext',
+		commonjsOptions: {
+			transformMixedEsModules: true
+		},
+		rollupOptions: {
+			plugins: [inject({ Buffer: ['buffer', 'Buffer'] }), nodePolyfills({ crypto: true })]
+		}
+	}
 };
 
 export default config;
